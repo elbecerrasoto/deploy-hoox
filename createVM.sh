4b5dfbcc5287738ed7fc403b224c61c2ed93978c
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-02-25 00:03:43 -0700

    createVM

--- createVM.sh
+++ createVM.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e  # Exit immediately if any command fails
+
 HOST='free-and-focus'
 USER="ebecerra"
 REMOTE="$USER@$HOST"
@@ -9,22 +11,37 @@ RAM='32000'
 DISK='200GB'
 ZONE='us-central1-c'
 
-SOURCE='install_hoox.sh'
-DESTINATION='install_hoox.sh'
+function cloud_exec {
+    gcloud compute ssh "$REMOTE" --zone="$ZONE" --command="$@"
+}
 
+function cloud_create {
+    echo "Creating VM: $HOST..."
+    gcloud compute instances create "$HOST" \
+        --zone="$ZONE" \
+        --machine-type=custom-"$CORES"-"$RAM" \
+        --image-family=debian-11 \
+        --image-project=debian-cloud \
+        --boot-disk-size="$DISK" \
+        --quiet
+}
 
-gcloud compute instances create "$HOST" \
-    --zone="$ZONE" \
-    --machine-type=custom-"$CORES"-"$RAM" \
-    --image-family=debian-11 \
-    --image-project=debian-cloud \
-    --boot-disk-size="$DISK"
+function cloud_delete {
+    echo "Deleting VM: $HOST..."
+    gcloud compute instances delete "$HOST" --zone="$ZONE" --quiet
+}
 
+# Check if the instance already exists
+if gcloud compute instances describe "$HOST" --zone="$ZONE" &>/dev/null; then
+    echo "Instance $HOST exists. Deleting and recreating..."
+    cloud_delete
+    cloud_create
+else
+    echo "Instance $HOST does not exist. Creating a new one..."
+    cloud_create
+fi
 
-gcloud compute ssh "$REMOTE" \
-    --zone="$ZONE" \
-    --command="sudo apt install -y git make"
-
-
-
-# gcloud compute instances delete "$HOST" --zone="$ZONE"
+# Execute setup commands on the VM
+cloud_exec "sudo apt update && sudo apt install -y git make"
+cloud_exec "git clone https://github.com/elbecerrasoto/deployment_hoox"
+cloud_exec "cd deployment_hoox && ./install_hoox"
